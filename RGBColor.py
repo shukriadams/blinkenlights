b69ae976404ddb6d8feeb5c94f2ee595511f4d4a
Shukri Adams <shukri.adams@gmail.com>
2019-09-09 12:34:43 +0200

    Update RGBColor.py

--- RGBColor.py
+++ RGBColor.py
@@ -1,13 +1,24 @@
 class RGBColor():
 
-    value = 0
-    mod = 0
-
-    def __init__(self, seed):
-        self.mod = seed
-
+    def __init__(self, speed):
+        self._speed = speed
+    
+    # do not access directly 
+    _value = 0
+    
+    # speed is the amount by which color changes per transition. Minimum should be 1, maximum should be something that's not so close
+    # to 255 that a step is not possible
+    _speed = 0
+    
+    # gets value 
+    def getValue(self):
+        return self._value
+    
     def increment(self):
-        if self.value + self.mod > 255 or self.value + self.mod < 0:
-            self.mod = self.mod * -1
         
-        self.value = self.value + self.mod
+        # change direction of transition if reach ceiling or floor
+        nextValue = self._value + self._speed
+        if nextValue > 255 or nextValue < 0:
+            self._speed = self._speed * -1
+        
+        self._value = self._value + self._speed
